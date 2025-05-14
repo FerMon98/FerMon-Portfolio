8ef699585aac7590ae89ef0c79b5117aa26eac4d
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2025-05-14 13:14:32 +0200

    Creating header

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Fernanda Montalvan">
+    <meta name="description" content="To-do, gestor de tareas">
+    <title>Fernanda's Portfolio</title>
+    <link rel="stylesheet" href="Resources/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <h1>Fernanda Montalvan</h1>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+</body>
+</html>
